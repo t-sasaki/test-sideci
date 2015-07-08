08492dac3604d4e04f0e61fd1c8a5708dea9ee70
t-sasaki <toshimitsu_sasaki@voyagegroup.com>
2015-07-08 21:51:06 +0900

    add SideCiSample7

---
+++ SideCiSample7.php
@@ -0,0 +1,10 @@
+<?php
+class SideCiSample7 {
+public function hoge() { echo "hoge"; }
+public function fuga() {
+echo "hoge";
+}
+                                                    public function piyo() {
+                                            echo "piyo";
+                                                    }
+}
